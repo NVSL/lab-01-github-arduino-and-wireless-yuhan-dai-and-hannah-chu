d021b3d9636d2d72decd88061cd466bbcd77766f
yuhan_d <yuhan.dai@tryhousecall.com>
2016-04-03 22:07:26 -0700

    Added Recevier and Transmitter floder

---
+++ BasicChat_Recevier/BasicChat_Recevier.ino
@@ -0,0 +1,61 @@
+/*
+  ATmega128RFA1 Dev Board Basic Chat
+  by: Jim Lindblom
+      SparkFun Electronics
+  date: July 3, 2012
+  License: Beerware. Feel free to use, reuse, and modify this code
+  as you please. If you find it useful, you can buy me a beer.
+
+  This code sets up the ATmega128RFA1's wireless transciever in
+  the most basic way possible to serve as a serial gateway.
+  Serial into the ATmega128RFA1's UART0 will go out the RF radio.
+  Data into the RF radio will go out the MCU's UART0.
+*/
+
+#include "RadioFunctions.h"
+
+int rec_data = 0;
+int data_size = 100;
+bool first = true;
+long beginT;
+long endT;
+bool done = false;
+
+void setup()
+{
+  Serial.begin(9600);  // Start up serial
+  Serial1.begin(115200);
+  rfBegin(15);  // Initialize ATmega128RFA1 radio on channel 11 (can be 11-26)
+  
+  // Send a message to other RF boards on this channel
+  rfPrint("ATmega128RFA1 Dev Board Online!\r\n");
+}
+
+void loop()
+{ 
+  if (rfAvailable())  // If data receievd on radio...
+  {
+    if(first) {
+      beginT = micros(); 
+      first = false;
+    }
+    // calculate the data that is recevide from recevier
+    if(rfRead() == 'p'){
+       rec_data++;
+    }
+    else{
+       endT = micros();
+       done = true;
+    }
+  }
+  else if(Serial.read() == 's' && done) 
+  {
+    Serial.print("data recevided: ");
+    Serial.println(rec_data);
+    Serial.print("Recevied Rate: ");
+    Serial.print(rec_data/(data_size*1.0));
+    Serial.println("%");
+    Serial.print("Time takes to recevie data: ");
+    Serial.println(endT-beginT);
+  }
+}
---
+++ BasicChat_Transmitter/BasicChat_Transmitter.ino
@@ -0,0 +1,52 @@
+/*
+  ATmega128RFA1 Dev Board Basic Chat
+  by: Jim Lindblom
+      SparkFun Electronics
+  date: July 3, 2012
+  License: Beerware. Feel free to use, reuse, and modify this code
+  as you please. If you find it useful, you can buy me a beer.
+
+  This code sets up the ATmega128RFA1's wireless transciever in
+  the most basic way possible to serve as a serial gateway.
+  Serial into the ATmega128RFA1's UART0 will go out the RF radio.
+  Data into the RF radio will go out the MCU's UART0.
+*/
+
+#include "RadioFunctions.h"
+
+int data_size = 100;
+int packet = 0;
+long time;
+long current;
+
+void setup()
+{
+  Serial.begin(9600);  // Start up serial
+  Serial1.begin(115200);
+  rfBegin(15);  // Initialize ATmega128RFA1 radio on channel 11 (can be 11-26)
+  
+  // Send a message to other RF boards on this channel
+  rfPrint("ATmega128RFA1 Dev Board Online!\r\n");
+  time = micros();
+}
+
+void loop()
+{  
+  if(Serial.available()){
+  // send out data_size characters through RFradio
+  if(packet < data_size) {
+    rfWrite('p');
+    packet++;   
+  } 
+  // calculate the time it takes to send all the data
+  else{
+    current = micros() - time;
+    Serial.print("Transfer rate for");
+    Serial.print(data_size);
+    Serial.print("packets:  ");
+    // calculate kbps
+    Serial.print(packet*8*1000/current);
+    Serial.println(" Kbps"); 
+  }
+  }
+}
